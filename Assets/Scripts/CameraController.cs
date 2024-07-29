57f27a21a134b8f310a73f099c1a8d21ebeb74d3
DualDura <jdsan216@gmail.com>
2024-07-29 20:45:06 +0900

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 1.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
