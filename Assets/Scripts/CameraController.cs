57560584cf0aa33a85a9c8e049ff350dcc86adda
DualDura <jdsan216@gmail.com>
2024-07-29 21:05:56 +0900

    카메라 회전 기능 추가
    + 카메라 컨트롤러 스크립트 조정

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,20 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float rotateSpeed = 1.0f;
-
-
+    [SerializeField] private float rotateSpeed = 1.0f;  //회전속도
+    [SerializeField] private Transform centerPos;       //회전의 중심
+    
     void Start()
     {
-        
+    
+    
     }
 
+    private void FixedUpdate()
+    {
+        transform.RotateAround(centerPos.transform.position, Vector3.up, Input.GetAxisRaw("CameraMovingAxis"));
+
+    }
 
     void Update()
     {
-        
     }
 }
