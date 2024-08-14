f2b905630c4bcd6af6974ebd608cafe8414f921e
DualDura <jdsan216@gmail.com>
2024-08-14 21:45:33 +0900

    필요한 에셋 임포트
    + 플레이어 관련 사운드 추가
    + 좀비 관련 사운드 추가
    + 무기 관련 사운드 추가

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    static private SoundManager instance;
+    static public SoundManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = FindAnyObjectByType<SoundManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        //BGM 재생
+    }
+
+}
