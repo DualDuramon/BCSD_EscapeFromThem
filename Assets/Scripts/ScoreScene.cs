0358459889f9a618e0991496a5f940d8addce9ab
DualDura <jdsan216@gmail.com>
2024-08-14 18:22:56 +0900

    타이틀 씬 구현
    + 타이틀 씬 생성
    + 타이틀 씬 용 애니메이션 생성

---
+++ Assets/Scripts/ScoreScene.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScene : MonoBehaviour
+{
+    [SerializeField] private Animator myAC;
+    [SerializeField] private Text heartScoreTxt;
+    [SerializeField] private Text bulletScoreTxt;
+    [SerializeField] private Text grenadeScoreTxt;
+    [SerializeField] private Text killScoreTxt;
+    [SerializeField] private Text totalScoreTxt;
+
+    [SerializeField] private int heartScore_Per_One;
+    [SerializeField] private int bulletScore_Per_One;
+    [SerializeField] private int grenadeScore_Per_One;
+    [SerializeField] private int killPoint_Per_One;
+    private int totalScore = 0;
+
+    void Start()
+    {
+        CalCulateScore();
+        StartScene();
+    }
+
+    private void StartScene()
+    {
+        myAC.SetTrigger("Start");
+    }
+
+    private void CalCulateScore()
+    {
+        //체력 점수 계산
+        int buffer = (int)GameManager.Instance.CurrentSaveData.nowHp * heartScore_Per_One;
+        heartScoreTxt.text = buffer.ToString();
+        totalScore += buffer;
+
+        //총알 점수 계산
+        buffer = (GameManager.Instance.CurrentSaveData.nowBullet_mag 
+            + GameManager.Instance.CurrentSaveData.nowBullet_reserve) 
+            * bulletScore_Per_One;
+        bulletScoreTxt.text = buffer.ToString();
+        totalScore += buffer;
+
+        //수류탄 점수 계산
+        buffer = GameManager.Instance.CurrentSaveData.nowGrenade * grenadeScore_Per_One;
+        grenadeScoreTxt.text = buffer.ToString();
+        totalScore += buffer;
+
+        //좀비 킬 점수 계산
+        buffer = GameManager.Instance.CurrentSaveData.totalZombieKills * killPoint_Per_One;
+        killScoreTxt.text = buffer.ToString();
+        totalScore += buffer;
+
+        //totalScore 텍스트 설정
+        totalScoreTxt.text = totalScore.ToString();
+    }
+    
+}
