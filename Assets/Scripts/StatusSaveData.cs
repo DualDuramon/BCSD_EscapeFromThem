9f0a6651b6f4af59f91d954855bb31e0d6c403c3
DualDura <jdsan216@gmail.com>
2024-08-17 18:06:25 +0900

    자잘한 버그 수정

--- Assets/Scripts/StatusSaveData.cs
+++ Assets/Scripts/StatusSaveData.cs
@@ -8,7 +8,7 @@ public class StatusSaveData
     public int nowStageIndex = 0;           //현재 스테이지 인덱스
     public float nowHp = 150.0f;            //현재 체력
     public int nowBullet_mag = 30;          //현재 탄창 탄약
-    public int nowBullet_reserve = 90;      //현재 예비 탄약
+    public int nowBullet_reserve = 150;      //현재 예비 탄약
     public int nowGrenade = 2;              //현재 수류탄
     public float nowWalkSpeed = 5.0f;       //현재 이동속도
     public int totalZombieKills = 0;        //좀비 킬 수
@@ -19,7 +19,7 @@ public class StatusSaveData
         nowStageIndex = 0;
         nowHp = 150.0f;
         nowBullet_mag = 30;
-        nowBullet_reserve = 90;
+        nowBullet_reserve = 150;
         nowGrenade = 2;
         nowWalkSpeed = 5.0f;
         totalZombieKills = 0;
