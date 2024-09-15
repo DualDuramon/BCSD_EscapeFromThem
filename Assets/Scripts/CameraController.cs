88be95575b5ea25e17a1cd0d644ad98c926be123
DualDura <jdsan216@gmail.com>
2024-09-15 13:52:27 +0900

    adjust GameSystem Value
    + add Guide Arrow
    + add Camera Rotate Speed
    + adjust Score calculate value

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform centerPos;       //회전의 중심
     //카메라 - 플레이어 offset 값 : (1.4, 9.44, -4.65)
+    [SerializeField] private Transform centerPos;       //회전의 중심
+    [SerializeField] private float rotateSpeed = 1.0f;  //회전 속도
 
 
     private void FixedUpdate()
     {
-        transform.RotateAround(centerPos.transform.position, Vector3.up, Input.GetAxisRaw("CameraMovingAxis"));
-
+        transform.RotateAround(centerPos.transform.position, Vector3.up, Input.GetAxisRaw("CameraMovingAxis")*rotateSpeed);
     }
 }
--- Assets/Scripts/StatusSaveData.cs
+++ Assets/Scripts/StatusSaveData.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class StatusSaveData
 {
     public int nowStageIndex = 0;           //현재 스테이지 인덱스
-    public float nowHp = 150.0f;            //현재 체력
+    public float nowHp = 100.0f;            //현재 체력
     public int nowBullet_mag = 30;          //현재 탄창 탄약
-    public int nowBullet_reserve = 150;      //현재 예비 탄약
+    public int nowBullet_reserve = 240;      //현재 예비 탄약
     public int nowGrenade = 2;              //현재 수류탄
     public float nowWalkSpeed = 5.0f;       //현재 이동속도
     public int totalZombieKills = 0;        //좀비 킬 수
@@ -17,9 +17,9 @@ public class StatusSaveData
     public void ResetData() //데이터 초기화 함수
     {
         nowStageIndex = 0;
-        nowHp = 150.0f;
+        nowHp = 100.0f;
         nowBullet_mag = 30;
-        nowBullet_reserve = 150;
+        nowBullet_reserve = 240;
         nowGrenade = 2;
         nowWalkSpeed = 5.0f;
         totalZombieKills = 0;
